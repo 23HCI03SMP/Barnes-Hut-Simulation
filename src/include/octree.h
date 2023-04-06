757b54d3ec2d2b3f0c270066b29730c303804140
OSdoge <ngyuheng08@gmail.com>
2023-04-07 01:01:10 +0800

    work on insertParticle()

---
+++ src/include/octree.h
@@ -0,0 +1,17 @@
+#include <list>
+#include <array>
+
+class Particle{
+    public:
+    float position[3]= {};
+};
+
+class bbox{
+    public:
+    
+    float box[3][2] = {
+        {},
+        {},
+        {}
+    };
+};
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "include/octree.h"
+#include <iostream>>
+int main(){
+    int boxX;
+    int boxY;
+    int boxZ;
+
+    std::cin >> boxX;
+    std::cin >> boxY;
+    std::cin >> boxZ;
+    int box[3] = {boxX, boxY, boxZ};
+
+    return 0;
+}
