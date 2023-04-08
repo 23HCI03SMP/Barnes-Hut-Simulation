1d8a2303fa9cd3498360719ac0d4ad1903d247e1
OSdoge <ngyuheng08@gmail.com>
2023-04-08 18:08:01 +0800

    rewrite octree code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ## Required libraries
 - GSL
+
+## To-do list
+
+- [ ] Finish creating Octree
+- [ ] Finish computation of particle acceleration
--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,6 @@ int main() {
     double a = pi();
     std::cout << a << std::endl;
 
-    Particle particle = {0, 0, 0};
-    Bbox box;
-    Node node;
-    node.insertParticle(particle, box);
-    for (int i = 0; i < (signed)(box.children).size(); i++){
-        std::cout << box.children[i].particle.position[0];
-        std::cout << box.children[i].particle.position[1];
-        std::cout << box.children[i].particle.position[1] << std::endl;
-    }
-
     
     return 0;
 }
