95b6e44de3defdcac3d29f8f305482d173c92fcf
UnidentifiedX <80146445+UnidentifiedX@users.noreply.github.com>
2023-04-29 11:22:02 +0800

    made a partially working initial conditions with gaussian distribution

--- src/initial_conditions.cpp
+++ src/initial_conditions.cpp
@@ -1,26 +1,57 @@
 #include <cmath>
 #include <stdexcept>
 #include "include/barnesHut.h"
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 
 std::vector<std::array<float, 4>> generatePoints(float minX, float minY, float minZ, float maxX, float maxY, float maxZ, float mass, float density)
 {
-    if (minX <= 0 || minY <= 0 || minZ <= 0)
-    {
-        throw std::invalid_argument("Coordinates cannot be negative");
-    }
-
     std::vector<std::array<float, 4>> points;
-    float phi, theta;
 
-    for (phi = 0; phi <= PI; phi += density) {
-        for (theta = 0; theta <= 2 * PI; theta += density) {
-            float x = sin(phi) * cos(theta) * (maxX - minX) / 2 + (maxX + minX) / 2;
-            float y = sin(phi) * sin(theta) * (maxY - minY) / 2 + (maxY + minY) / 2;
-            float z = cos(phi) * (maxZ - minZ) / 2 + (maxZ + minZ) / 2;
+    float rangeX = maxX - minX;
+    float rangeY = maxY - minY;
+    float rangeZ = maxZ - minZ;
+    float minRange = std::min(std::min(rangeX, rangeY), rangeZ);
+    
+    float radius = minRange / 2.0f;
+    int numPoints = std::ceil(density * 4.0f * PI * std::pow(radius, 3.0f) / 3.0f);
 
-            points.push_back({x, y, z, mass});
-        }
+    gsl_rng* rng = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set(rng, time(NULL));
+
+    for (int i = 0; i < numPoints; ++i)
+    {
+        float x, y, z;
+        do {
+            x = minX + gsl_rng_uniform(rng) * rangeX;
+            y = minY + gsl_rng_uniform(rng) * rangeY;
+            z = minZ + gsl_rng_uniform(rng) * rangeZ;
+        } while (pow(x - 3, 2) + pow(y - 3, 2) + pow(z - 3, 2) > pow(radius, 2));
+
+        points.push_back({x, y, z, mass});
     }
+
+    gsl_rng_free(rng);
 
     return points;
+
+    // if (minX <= 0 || minY <= 0 || minZ <= 0)
+    // {
+    //     throw std::invalid_argument("Coordinates cannot be negative");
+    // }
+
+    // std::vector<std::array<float, 4>> points;
+    // float phi, theta;
+
+    // for (phi = 0; phi <= PI; phi += density) {
+    //     for (theta = 0; theta <= 2 * PI; theta += density) {
+    //         float x = sin(phi) * cos(theta) * (maxX - minX) / 2 + (maxX + minX) / 2;
+    //         float y = sin(phi) * sin(theta) * (maxY - minY) / 2 + (maxY + minY) / 2;
+    //         float z = cos(phi) * (maxZ - minZ) / 2 + (maxZ + minZ) / 2;
+
+    //         points.push_back({x, y, z, mass});
+    //     }
+    // }
+
+    // return points;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-    auto points = generatePoints(1, 1, 1, 5, 5, 5, 1, 0.1);
+    auto points = generatePoints(1, 1, 1, 5, 5, 5, 1, 100);
     generateFile(points);
 
-    std::vector<std::array<float, 4>> a = loadInitialValues();
+    // std::vector<std::array<float, 4>> a = loadInitialValues();
 
-    Octree tree = Octree(1, 1, 1, 5, 5, 5);
-    Octree *tree_ptr = &tree;
+    // Octree tree = Octree(1, 1, 1, 5, 5, 5);
+    // Octree *tree_ptr = &tree;
 
     // tree.insert(tree_ptr, 3.84147, 3.4597, 4.75517, 1, 1);
     // tree.insert(tree_ptr, 2.10208, 2.66365, 4.75517, 1, 1);
@@ -18,10 +18,10 @@ int main()
     // tree.insert(tree_ptr, 3.27199, 2.08053, 4.75517, 1, 1);
     // tree.insert(tree_ptr, 3.67951, 2.32349, 4.75517, 1, 1);
 
-    for (std::array<float, 4> point: a)
-    {
-        tree.insert(tree_ptr, point[0], point[1], point[2], point[3], point[3]);
-    }
+    // for (std::array<float, 4> point: a)
+    // {
+    //     tree.insert(tree_ptr, point[0], point[1], point[2], point[3], point[3]);
+    // }
 
     // tree.insert(1, 1, 1, 1);
     // tree.insert(tree_ptr, 5, 5, 5, 100);
@@ -44,11 +44,11 @@ int main()
 
     // barnes.cpp
 
-    Barnes barnes;
-    barnes.calcForce(tree_ptr->children[0], tree_ptr->children[7], 0.5);
+    // Barnes barnes;
+    // barnes.calcForce(tree_ptr->children[0], tree_ptr->children[7], 0.5);
 
-    Simulation sim = Simulation();
-    Octree final = sim.mainLoop(tree_ptr, 1, 0.5);
+    // Simulation sim = Simulation();
+    // Octree final = sim.mainLoop(tree_ptr, 1, 0.5);
 
     std::getchar();
     return 0;
