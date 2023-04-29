09625d5ff434c8b7d320a851f1a88295f3a85d35
UnidentifiedX <80146445+UnidentifiedX@users.noreply.github.com>
2023-04-29 22:48:53 +0800

    changed to using a for loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "include/barnesHut.h"
 #include <iostream>
 
-Octree *loop(int iteration, Octree *octree)
+Octree *loop(Octree *octree, int iterations, float timeStep)
 {
     Octree *finalPtr;
 
-    while (iteration != 0)
+    for (int i = iterations; i >= 0; i--)
     {
         std::vector<Octree *> childVect = getChildren(octree);
         Barnes barnes;
@@ -20,32 +20,24 @@ Octree *loop(int iteration, Octree *octree)
         }
 
         Simulation sim = Simulation();
-        Octree final = sim.mainLoop(octree, 1, 1e-5);
+        Octree final = sim.mainLoop(octree, 1, timeStep);
         finalPtr = &final;
-
-        iteration--;
     }
+
     return finalPtr;
 }
 
 int main()
 {
-    std::vector<std::array<float, 7>> points = generatePoints(1, 1, 1, 5, 5, 5, 1, 100, 293); // 293K = 20C
+    std::vector<std::array<float, 7>> points = generatePoints(1, 1, 1, 5, 5, 5, 1, 20, 293); // 293K = 20C
     generateFile(points);
-
 
     std::vector<std::array<float, 7>> a = loadInitialValues();
 
     Octree tree = Octree(1, 1, 1, 5, 5, 5);
     Octree *tree_ptr = &tree;
 
-    // tree.insert(tree_ptr, 1, 1, 1, 1, 1);
-    // tree.insert(tree_ptr, 5, 5, 5, 1, 1);
-    // tree.insert(tree_ptr, 2.79788, 2.06269, 4.75517, 1, 1);
-    // tree.insert(tree_ptr, 3.27199, 2.08053, 4.75517, 1, 1);
-    // tree.insert(tree_ptr, 3.67951, 2.32349, 4.75517, 1, 1);
-
-    for (std::array<float, 7> point: a)
+    for (std::array<float, 7> point : a)
     {
         float x = point[0];
         float y = point[1];
@@ -58,27 +50,7 @@ int main()
         tree.insert(tree_ptr, x, y, z, vx, vy, vz, mass, mass);
     }
 
-    // tree.insert(1, 1, 1, 1);
-    // tree.insert(tree_ptr, 5, 5, 5, 100);
-
-    // std::cout << (tree.find(2, 1, 1)
-    //                   ? "Found\n"
-    //                   : "Not Found\n");
-
-    // std::cout << (tree.find(1, 2, 3)
-    //                   ? "Found\n"
-    //                   : "Not Found\n");
-
-    // std::cout << (tree.find(3, 4, 4)
-    //                   ? "Found\n"
-    //                   : "Not Found\n");
-
-    // std::cout << (tree.find(3, 4, 4)
-    //                   ? "Found\n"
-    //                   : "Not Found\n");
-
-    // barnes.cpp
-    Octree *final = loop(1, tree_ptr);
+    Octree *final = loop(tree_ptr, 1, 1e-10);
     std::getchar();
     return 0;
 }
