42093fb538c1680540d3d3d349397899900ffa16
OSdoge <ngyuheng08@gmail.com>
2023-06-13 16:36:06 +0800

    .

--- src/main.cpp
+++ src/main.cpp
@@ -12,23 +12,30 @@ void loop(Octree *octree, int iterations, float theta, float timeStep)
 
     for (int i = 0; i <= iterations; i++)
     {
+        // Start timer
         auto start = high_resolution_clock::now();
-        std::vector<Octree *> childVect = getChildren(final);
-        Barnes barnes;
 
+        // Get all particles in octree
+        std::vector<Octree *> childVect = getChildren(final);
+
+        Barnes barnes;
         for (Octree *child : childVect)
         {
+            // calculate and update forces/fields
             barnes.calcForce(final, child, theta);
         }
 
+        // Stop timer
         auto stop = high_resolution_clock::now();
         auto duration = duration_cast<milliseconds>(stop - start);
         totalDur += duration.count();
         std::cout << duration.count() << " ms " << std::endl;
 
+        // Update positions and velocities
         Simulation sim = Simulation();
         sim.mainLoop(final, timeStep);
 
+        // Update simulation_values.csv file
         generateSimulationValuesFile(final);
     }
 
@@ -37,31 +44,26 @@ void loop(Octree *octree, int iterations, float theta, float timeStep)
 
 int main()
 {
+    // Define particles in plasma
     std::vector<InsertedParticle> particles = {
         InsertedParticle("Deutron", 2, 1, 0.5),
         InsertedParticle("Electron", 1/1823.0f, -1, 0.5),
     };
 
-    // std::vector<InsertedParticle> particles = {
-    //     InsertedParticle("Deutron", M_PROTON + M_NEUTRON, 1, 0.5),
-    //     InsertedParticle("Electron", M_ELECTRON, 1, 0.5),
-    // };
-
-    std::cout << "Starting simulation..." << std::endl;
-
+    // Create Octree object
     Octree tree = Octree(0, 0, 0, 20, 20, 20);
     Octree *tree_ptr = &tree;
 
-    std::vector<CSVPoint> points = generateInitialPoints(tree_ptr, 2, 2, 2, 20, 294, particles, Shape::SPHERE); // 293K = 20C
-    tree_ptr->recalculateCenterOfCharge(tree_ptr);
-    // std::vector<CSVPoint> points = generateInitialPoints(tree_ptr, 2, 2, 10, 100, 294, particles, Shape::REGULAR_CYLINDER); // 293K = 20C
+    // Generate initial points
+    //std::vector<CSVPoint> points = generateInitialPoints(tree_ptr, 2, 2, 10, 100, 294, particles, Shape::REGULAR_CYLINDER); // 293K = 20C
+    tree.insert(tree_ptr, "Deutron", 1, 1, 1, 1, 1, 1, 1, 1);
 
-    loop(tree_ptr, 100, 0, 1e-8);
+    // Start simulation loop
+    loop(tree_ptr, 50, 0, 1e10);
 
-    std::cout << "\nAnimator Starting...\n";
+    // Animate and generate .vtk files
     system("py ./animator.py");
-
-    //tester();
+    system("py ./vtk_animator.py");
 
     return 0;
 }
